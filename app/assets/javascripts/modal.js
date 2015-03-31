2d7936f7eefbf08f5df316a4568eede51ccb9e70
gogojojo <joseph.gochua@gmail.com>
2015-03-31 14:52:53 -0700

    added modal for login and signup

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -2,4 +2,10 @@ $( function () {
   $( "#open_post" ).click(function() {
   $( "#post_modal").removeClass('hidden')
 });
+  $( "#open_signup" ).click(function() {
+  $( "#user_modal").removeClass('hidden')
+});
+   $( "#open_login" ).click(function() {
+  $( "#sessions_modal").removeClass('hidden')
+});
 })
