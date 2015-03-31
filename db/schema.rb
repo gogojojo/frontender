9312673db7d0c69423ed12ad0ca3fffafc1a91fe
gogojojo <joseph.gochua@gmail.com>
2015-03-31 01:01:36 -0700

    added user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150331005834) do
+ActiveRecord::Schema.define(version: 20150331080058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,14 @@ ActiveRecord::Schema.define(version: 20150331005834) do
   end
 
   add_index "posts", ["day_id"], name: "index_posts_on_day_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   add_foreign_key "posts", "days"
 end
