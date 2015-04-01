ebf8f373a9d4091e3f004680489fb39766b374e0
gogojojo <joseph.gochua@gmail.com>
2015-03-31 20:29:35 -0700

    finished modal/

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -2,6 +2,9 @@ $( function () {
   $( "#open_post" ).click(function() {
   $( "#post_modal").removeClass('hidden')
 });
+  $( "#close_post" ).click(function() {
+  $( "#post_modal").addClass('hidden')
+});
   $( "#open_signup" ).click(function() {
   $( "#user_modal").removeClass('hidden')
 });
