311a1fd0b3613d3d5183a49b89ab7b97dd24b5f3
gogojojo <joseph.gochua@gmail.com>
2015-03-31 20:50:01 -0700

    relations between posts and day works

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,26 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
-  
+
   def new
     @post = Post.new
   end
 
   def create
     @post = Post.new(params.require(:post).permit(:title, :url, :description))
+    if @post.save
+      if Day.find_by(date: @post.created_at.strftime('%b %d %Y'))
+        @day = Day.find_by(date: @post.created_at.strftime('%b %d %Y'))
+        @post.day_id = @day.id 
+        @post.save
+        redirect_to '/'
+      else
+        @day = Day.create(date: @post.created_at.strftime('%b %d %Y') )
+        @post.day_id = @day.id
+        @post.save
+        redirect_to '/'
+      end
+    end
   end
+
 end
--- app/models/day.rb
+++ app/models/day.rb
@@ -1,3 +1,3 @@
 class Day < ActiveRecord::Base
-  has_many: posts
+  has_many :posts
 end
