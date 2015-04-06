0e9cc0452905a049a6d7f88684d740e92bb18309
gogojojo <joseph.gochua@gmail.com>
2015-04-05 20:21:58 -0700

    fixing routing error

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/' => 'posts#index'
   get '/logout' => 'sessions#destroy'
   get '/login12345678910' => 'sessions#new'
-  post '/login' => 'sessions#create'
+  post '/login12345678910' => 'sessions#create'
   get "/upvote" => 'posts#upvote', as: 'upvote'
   get "/downvote" => 'posts#downvote', as: 'downvote'
 end
