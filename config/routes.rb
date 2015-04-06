ade576bfdfb49dad6cd8118e77d8403683dc3333
gogojojo <joseph.gochua@gmail.com>
2015-04-05 20:13:16 -0700

    got rid of users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   resources :posts
-  resources :users 
   resources :sessions
   get '/' => 'posts#index'
   get '/logout' => 'sessions#destroy'
-  get '/login' => 'sessions#new'
+  get '/login12345678910' => 'sessions#new'
   post '/login' => 'sessions#create'
   get "/upvote" => 'posts#upvote', as: 'upvote'
   get "/downvote" => 'posts#downvote', as: 'downvote'
